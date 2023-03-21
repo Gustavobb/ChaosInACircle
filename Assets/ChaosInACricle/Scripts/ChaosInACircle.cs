cffa05a37e35ac814894e53168fb576a51d7cade
Gustavo Beltrão Braga <38335492+Gustavobb@users.noreply.github.com>
2023-03-21 18:34:47 -0300

    Updates

---
+++ Assets/ChaosInACricle/Scripts/ChaosInACircle.cs
@@ -0,0 +1,145 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChaosInACircle : MonoBehaviour
+{
+    [SerializeField] private Material _material;
+    [SerializeField] private RawImage _image;
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private AudioClip _audioClip;
+
+    [Header("Setup")]
+    [SerializeField] private int _numCircles = 10;
+    [SerializeField] private float _audioMaxCooldown = 0.01f;
+    [SerializeField] private float _gravity = 0.1f;
+    [SerializeField] private float _circleRadius = 0.01f;
+    [SerializeField] private float _circumferenceOuterRadius = 0.505f;
+    [SerializeField] private float _circumferenceInnerRadius = 0.5f;
+    [SerializeField] private float _smoothness = 0.0f;
+
+    private Vector4[] _centers = new Vector4[100];
+    private Vector4[] _velocities = new Vector4[100];
+    private Color[] _colors = new Color[100];
+    private float[] _radius = new float[100];
+
+    private float _audioCooldown = 0;
+    [SerializeField] private RenderTexture _previousFrameTex;
+
+    private void Start()
+    {
+        CreateRenderTexture();
+        Reset();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            Reset();
+        
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            float random = Random.Range(0f, 1f);
+            _material.SetFloat("_ColorDecay", random < 0.5f ? 0.002f : 0f);
+
+            _numCircles = Random.Range(2, 40);
+            _circleRadius = Random.Range(0.005f, 0.03f);
+            _gravity = Random.Range(0.1f, 0.7f);
+            Reset();
+        }
+
+        if (Input.GetKeyDown(KeyCode.M))
+            _audioSource.mute = !_audioSource.mute;
+    }
+
+    private void FixedUpdate()
+    {
+        float aspect = (float)Screen.width / (float)Screen.height;
+        _material.SetFloat("_Aspect", aspect);
+        _material.SetInt("_NumCircles", _numCircles);
+        _material.SetVector("_CircumferenceRadius", new Vector2(_circumferenceInnerRadius, _circumferenceOuterRadius));
+        _material.SetFloat("_Smoothness", _smoothness);
+
+        UpdateCircles();
+    }
+
+    private void CreateRenderTexture()
+    {
+        if (_previousFrameTex != null)
+            _previousFrameTex.Release();
+
+        _previousFrameTex = new RenderTexture(Screen.width, Screen.height, 0, RenderTextureFormat.ARGB32);
+        _previousFrameTex.Create();
+    }
+
+    private void CreateCircle(int k)
+    {
+        float xOffset = .5f + (k - _numCircles / 2) * .0001f;
+        _centers[k] = new Vector4(xOffset, .5f, 0, 0);
+        _velocities[k] = Vector4.zero;
+        _colors[k] = Random.ColorHSV(0f, 1f, .5f, 1f, 1f, 1f);
+        _radius[k] = _circleRadius;
+    }
+
+    [EButton]
+    private void Reset()
+    {
+        _centers = new Vector4[100];
+        _velocities = new Vector4[100];
+        _colors = new Color[100];
+        _radius = new float[100];
+
+        for (int k = 0; k < _numCircles; k++)
+            CreateCircle(k);
+    }
+
+    private void UpdateCircle(int k)
+    {
+        Vector4 velocity = _velocities[k];
+        Vector4 center = _centers[k];
+        float radii = _radius[k];
+
+        velocity.y -= _gravity * Time.fixedDeltaTime;
+
+        Vector4 circumferenceCenter = new Vector4(0.5f, 0.5f, 0, 0);
+        Vector4 toCenter = center - circumferenceCenter;
+        float pos = toCenter.x * toCenter.x + toCenter.y * toCenter.y;
+        float radius = _circumferenceInnerRadius - radii * 1.5f;
+
+        if (pos >= radius * radius)
+        {
+            velocity = Vector2.Reflect(velocity, toCenter.normalized);
+            
+            if (_audioCooldown <= 0)
+            {
+                _audioSource.pitch = Random.Range(0.8f, 1.2f);
+                _audioSource.PlayOneShot(_audioClip);
+            }
+
+            _audioCooldown = _audioMaxCooldown;
+        }
+
+        center += velocity * Time.fixedDeltaTime;
+        _centers[k] = center;
+        _velocities[k] = velocity;
+    }
+
+    private void UpdateCircles()
+    {
+          for (int k = 0; k < _numCircles; k++)
+            UpdateCircle(k);
+
+        _audioCooldown -= Time.fixedDeltaTime;
+        _material.SetVectorArray("_Centers", _centers);
+        _material.SetColorArray("_Colors", _colors);
+        _material.SetFloatArray("_Radius", _radius);
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        _material.SetTexture("_PrevFrame", _previousFrameTex);
+        Graphics.Blit(source, destination, _material);
+        Graphics.Blit(destination, _previousFrameTex);
+    }
+}
---
+++ Assets/ChaosInACricle/Shaders/ChaosInACircle.shader
@@ -0,0 +1,102 @@
+Shader "Custom/ChaosInACircle"
+{
+    Properties
+    {
+        _MainTex ("Base (RGB)", 2D) = "white" {}
+        _PrevFrame ("Previous Frame", 2D) = "white" {}
+        _Aspect ("Aspect", float) = 1.0
+        _Smoothness ("Smoothness", Range(0, 1)) = 0.01
+        _ColorDecay ("Color Decay", Range(0, 1)) = 0.01
+        _NumCircles ("Number of Circles", Range(0, 100)) = 0
+        [ShowAsVector2] _CircumferenceRadius ("Circumference Radius", vector) = (0.5, 0.505, 0, 0)
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+
+        // No culling or depth
+        Cull Off ZWrite Off ZTest Always
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            struct Circle
+            {
+                float2 center;
+                float2 velocity;
+                float4 color;
+                float radius;
+            };
+
+            float _Aspect;
+            float _Smoothness;
+            float _ColorDecay;
+            float _NumCircles;
+            float2 _CircumferenceRadius;
+
+            float4 _Centers[100];
+            float4 _Colors[100];
+            float _Radius[100];
+
+            sampler2D _PrevFrame;
+            sampler2D _MainTex;
+            
+            float4 DrawCircle(float2 cuv, float2 uv, float innerRadius, float outerRadius, float4 color)
+            {
+                float2 diff = uv - cuv;
+                diff.x *= _Aspect;
+                float dist = length(diff);
+                float alpha = smoothstep(outerRadius, outerRadius - _Smoothness, dist);
+                alpha -= smoothstep(innerRadius, innerRadius - _Smoothness, dist);
+                return color * alpha;
+            }
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                float4 circunference = DrawCircle(float2(0.5, 0.5), i.uv, _CircumferenceRadius.x, _CircumferenceRadius.y, float4(1, 1, 1, 1));
+                float4 prevColor = tex2D(_PrevFrame, float2(i.uv.x, 1 - i.uv.y));
+                float4 color = 0;
+
+                for (int k = 0; k < _NumCircles; k++)
+                {
+                    float4 c = DrawCircle(_Centers[k], i.uv, 0, _Radius[k], _Colors[k]);
+                    color += c;
+                }
+
+                if (length(color) == 0)
+                    color = prevColor - _ColorDecay;
+                    
+                color += circunference;
+                return saturate(color);
+            }
+            ENDCG
+        }
+    }
+
+    FallBack "Diffuse"
+}
